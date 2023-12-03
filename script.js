f131e3f7454564d29b1e238313e1f08c105a5c35
colecrum26 <cwcrumrine@gmail.com>
2023-12-03 14:06:47 -0600

    refactored event to use click

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const input = document.getElementById("type-here");
 const output = document.getElementById("output");
+const goBtn = document.getElementById("go-btn");
 
-input.addEventListener("change", (e) => {
-  let name = e.target.value;
+goBtn.addEventListener("click", () => {
+  let name = input.value;
   if (
     name.charAt(name.length - 1) === "s" ||
     name.charAt(name.length - 1) === "x"
@@ -11,4 +12,4 @@ input.addEventListener("change", (e) => {
   } else {
     return (output.innerHTML = name.charAt(0).toUpperCase() + name.slice(1) + "s");
   }
-});
+})
