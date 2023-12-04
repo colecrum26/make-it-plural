c543c6e224c126be75a32e2bfae6b0bd5c46ad1b
colecrum26 <cwcrumrine@gmail.com>
2023-12-03 23:36:31 -0600

    added another edge case to script; adjusted io height to account for increase in input text size

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const goBtn = document.getElementById("go-btn");
 
 goBtn.addEventListener("click", () => {
   let name = input.value;
-  if (name.slice(-4) === "eaux") {
-    return (output.innerHTML =
-      name.charAt(0).toUpperCase() + name.slice(1) + "s");
-  } else if (
-    name.charAt(name.length - 1) === "s" ||
-    name.charAt(name.length - 1) === "x" ||
-    name.charAt(name.length - 1) === "z" ||
-    name.slice(-2) === "ch" ||
-    name.slice(-2) === "zh"
-  ) {
-    return (output.innerHTML =
-      name.charAt(0).toUpperCase() + name.slice(1) + "es");
+  if (name) {
+    if (name.slice(-4) === "eaux") {
+      return (output.innerHTML =
+        name.charAt(0).toUpperCase() + name.slice(1) + "s");
+    } else if (
+      name.charAt(name.length - 1) === "s" ||
+      name.charAt(name.length - 1) === "x" ||
+      name.charAt(name.length - 1) === "z" ||
+      name.slice(-2) === "ch" ||
+      name.slice(-2) === "zh"
+    ) {
+      return (output.innerHTML =
+        name.charAt(0).toUpperCase() + name.slice(1) + "es");
+    } else {
+      return (output.innerHTML =
+        name.charAt(0).toUpperCase() + name.slice(1) + "s");
+    }
   } else {
-    return (output.innerHTML =
-      name.charAt(0).toUpperCase() + name.slice(1) + "s");
+     window.alert("Whoops! Please enter a name or word.")
   }
 });
